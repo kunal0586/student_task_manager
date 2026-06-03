35a82669e651df5c68284e0ae07ffc3110ec6ecf
kunal0586 <kunal05088@gmail.com>
2026-06-03 09:50:03 +0530

    Updated app and database config

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def add_student():
         cursor.close()
         connection.close()
 
-        return "Student added successfully!"
+        return redirect('/students')
     return render_template('add_student.html')
 
 
--- db_config.py
+++ db_config.py
@@ -2,12 +2,24 @@
 import mysql.connector
 
 
+
 def get_database_connection():
     connection = mysql.connector.connect(
-        host='localhost', 
-        user='root', 
-        password='Kunal@2026',
+        host='gateway01.ap-southeast-1.prod.aws.tidbcloud.com', 
+        user='XxbCGG5JLegsM59.root', 
+        password='37HtCKSbBSYXblK2',
         database='student_task_manager',
-        auth_plugin='mysql_native_password'
+        port = 4000 
     )
     return connection
+
+
+# def get_database_connection():
+#     connection = mysql.connector.connect(
+#         host='localhost', 
+#         user='root', 
+#         password='Kunal@2026',
+#         database='student_task_manager',
+#         auth_plugin='mysql_native_password'
+#     )
+#     return connection
